b4b32e6ce337b966846f6dd9593d8bddea6a80d4
Sanganh <71274596+Sanganh@users.noreply.github.com>
2020-10-11 16:28:37 +0700

    edit/sanganh

--- QuanLyTruongHoc/QuanLyTruongHoc/User/StudentUC.cs
+++ QuanLyTruongHoc/QuanLyTruongHoc/User/StudentUC.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using QuanLyTruongHoc.EF;
 using QuanLyTruongHoc.Function;
+using System.Threading;
 
 namespace QuanLyTruongHoc.User
 {
@@ -20,16 +21,35 @@ namespace QuanLyTruongHoc.User
             List<StudentEF> listStudent = new StudentFt().SelectAll();
             Load(listStudent);
         }
+
         void Load(List<StudentEF> listStudent)
         {
             dgView.Rows.Clear();
             int index = 1;
-            foreach (StudentEF item in listStudent)
+            foreach(StudentEF item in listStudent)
             {
-                dgView.Rows.Add(index.ToString(), null, item.StudentCode, item.Sex, item.DateOfBirth.ToShortDateString());
+                string sex = item.Sex == true ? "Nam" : "Nữ";
+                dgView.Rows.Add(index.ToString(), item.StudentCode, item.FullName, sex, String.Format("{0:dd/MM/yyyy}",item.DateOfBirth));
                 index++;
             }
         }
+        private void dgView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1)
+            {
+                return;
+            }
+            string StudentCode = dgView.Rows[e.RowIndex].Cells["StudentCode"].Value.ToString();
+            StudentEF Student = new StudentFt().SelectAll().Where(x => x.StudentCode.ToString() == StudentCode).ToList().FirstOrDefault();
+            txbCode.Text = StudentCode;
+            txbName.Text = Student.FullName;
+            dtDateOfBirth.Value = Student.DateOfBirth;
+            cbSex.SelectedIndex = Student.Sex == true ? 0 : 1;
+            ArrByte_Anh = Student.Avatar;
+            picAvt.Image = User.ImageHandle.ByteToImage(ArrByte_Anh);
+            txbAddress.Text = Student.Address;
+        }
+
         private void btAdd_Click(object sender, EventArgs e)
         {
             if (txbCode.Text == "" || txbName.Text == "")
@@ -49,9 +69,12 @@ namespace QuanLyTruongHoc.User
             Student.Sex = cbSex.SelectedIndex == 0 ? true : false;
             Student.DateOfBirth = dtDateOfBirth.Value;
             Student.Address = txbAddress.Text;
+            Student.Avatar = ArrByte_Anh;
             bool result = new StudentFt().Insert(Student);
             if (result == true)
             {
+                List<StudentEF> listStudent = new StudentFt().SelectAll();
+                Load(listStudent);
                 MessageBox.Show("Thêm thành công học sinh", "Thông báo");
             }
             else
@@ -73,9 +96,12 @@ namespace QuanLyTruongHoc.User
             Student.Sex = cbSex.SelectedIndex == 0 ? true : false;
             Student.DateOfBirth = dtDateOfBirth.Value;
             Student.Address = txbAddress.Text;
+            Student.Avatar = ArrByte_Anh;
             bool result = new StudentFt().Update(Student);
             if (result == true)
             {
+                List<StudentEF> listStudent = new StudentFt().SelectAll();
+                Load(listStudent);
                 MessageBox.Show("Sửa thành công học sinh", "Thông báo");
             }
             else
@@ -97,9 +123,12 @@ namespace QuanLyTruongHoc.User
             Student.Sex = cbSex.SelectedIndex == 0 ? true : false;
             Student.DateOfBirth = dtDateOfBirth.Value;
             Student.Address = txbAddress.Text;
+            Student.Avatar = ArrByte_Anh;
             bool result = new StudentFt().Delete(Student);
             if (result == true)
             {
+                List<StudentEF> listStudent = new StudentFt().SelectAll();
+                Load(listStudent);
                 MessageBox.Show("Xóa thành công học sinh", "Thông báo");
             }
             else
@@ -118,19 +147,42 @@ namespace QuanLyTruongHoc.User
             List<StudentEF> listStudent = new StudentFt().SelectAll().Where(x => x.FullName == text || x.StudentCode.ToString() == text || x.Address == text).ToList();
             Load(listStudent);
         }
-
-        private void dgView_CellClick(object sender, DataGridViewCellEventArgs e)
+        byte[] ArrByte_Anh;
+        private void picAvt_DoubleClick(object sender, EventArgs e)
         {
-            if (e.RowIndex == -1)
+            string filename = "";
+            Thread thr = new Thread((ThreadStart)(() =>
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Multiselect = false;
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    filename = open.FileName.ToString();
+                }
+            }));
+            thr.SetApartmentState(ApartmentState.STA);
+            thr.Start();
+            thr.Join();
+
+            if (filename == "")
             {
                 return;
             }
-            string StudentCode = dgView.Rows[e.RowIndex].Cells["StudentCode"].Value.ToString();
-            StudentEF Student = new StudentFt().SelectAll().Where(x => x.StudentCode.ToString() == StudentCode).ToList().FirstOrDefault();
-            txbCode.Text = StudentCode;
-            txbName.Text = Student.FullName;
-            dtDateOfBirth.Value = Student.DateOfBirth;
-            cbSex.SelectedIndex = Student.Sex == true ? 0 : 1;
+
+            byte[] arrByte = ImageHandle.StringToByte(filename);
+            ArrByte_Anh = arrByte;
+
+            picAvt.Image = ImageHandle.ByteToImage(arrByte);
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            picAvt.Image = null;
+            txbCode.Text = "";
+            txbName.Text = "";
+            txbAddress.Text = "";
+            cbSex.SelectedIndex = 0;
+            ArrByte_Anh = null;
         }
     }
 }
