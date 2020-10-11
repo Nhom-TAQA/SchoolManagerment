82b043eb910364d2056afc3fef6d0eed6b50afaf
Nguyễn Trọng Quyết <59854190+Quyet1999@users.noreply.github.com>
2020-10-11 08:13:53 +0700

    Create Main.cs

---
+++ QuanLyTruongHoc/QuanLyTruongHoc/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyTruongHoc.User;
+
+namespace QuanLyTruongHoc
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new Home());
+        }
+
+        private void btTeacher_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new TeacherUC(pnMain.Width,pnMain.Height));
+        }
+
+        private void btStudent_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new StudentUC());
+        }
+
+        private void btClass_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new ClassUC());
+        }
+
+        private void btHomeroomTeacher_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new HomeroomTeacherUC());
+        }
+
+        private void btStudentInClass_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new StudentInClassUC());
+        }
+
+        private void btLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btHome_Click(object sender, EventArgs e)
+        {
+            pnMain.Controls.Clear();
+            pnMain.Controls.Add(new Home());
+        }
+    }
+}
