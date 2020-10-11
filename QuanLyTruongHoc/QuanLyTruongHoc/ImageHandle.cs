cc739f7bb765d0831911638525a416178fdc1dbe
andrea511 <vuhoanganhhvgl@gmail.com>
2020-10-11 15:34:37 +0700

    update/by-hoang anh
    hoàng anh update

---
+++ QuanLyTruongHoc/QuanLyTruongHoc/ImageHandle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace QuanLyTruongHoc.User
+{
+    public static class ImageHandle
+    {
+        public static Image ByteToImage(byte[] arrImage)
+        {
+            if (arrImage == null)
+            {
+                return null;
+            }
+            MemoryStream ms = new MemoryStream(arrImage, 0, arrImage.Length);
+            ms.Write(arrImage, 0, arrImage.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+        public static byte[] StringToByte(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] arrImage = new byte[fs.Length];
+            fs.Read(arrImage, 0, Convert.ToInt32(fs.Length));
+            fs.Close();
+            return arrImage;
+        }
+    }
+}
