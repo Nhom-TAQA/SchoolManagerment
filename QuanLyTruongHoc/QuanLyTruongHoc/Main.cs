e2698d23d8de37a96b58c8c08be33cbf605f0600
Nguyễn Trọng Quyết <59854190+Quyet1999@users.noreply.github.com>
2020-12-04 08:53:38 +0700

    Update Main.cs

--- QuanLyTruongHoc/QuanLyTruongHoc/Main.cs
+++ QuanLyTruongHoc/QuanLyTruongHoc/Main.cs
@@ -29,8 +29,7 @@ namespace QuanLyTruongHoc
 
         private void btStudent_Click(object sender, EventArgs e)
         {
-            pnMain.Controls.Clear();
-            pnMain.Controls.Add(new StudentUC());
+          
         }
 
         private void btClass_Click(object sender, EventArgs e)
