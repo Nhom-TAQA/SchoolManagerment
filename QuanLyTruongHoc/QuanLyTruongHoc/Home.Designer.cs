e0e61e0ff467699649da7e2f3b829e574c1b6e6b
Nguyễn Trọng Quyết <59854190+Quyet1999@users.noreply.github.com>
2020-12-11 10:30:11 +0700

    /quyết

--- QuanLyTruongHoc/QuanLyTruongHoc/Home.Designer.cs
+++ QuanLyTruongHoc/QuanLyTruongHoc/Home.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Home";
+            this.Size = new System.Drawing.Size(1037, 612);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
